81e40f61b24a34519064e8529f7169cb18f3c1fb
Roit Jung Karki <roit.github@gmail.com>
2024-03-22 22:15:16 +0545

    basic Canvas setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+import Canvas from './components/Canvas';
+import ServicePane from './components/ServicePane';
 import './App.css';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cantainer'>
+      <ServicePane />
+      <Canvas>
+        <Text text="Some text on canvas" fontSize={15} />
+        <Rect
+          x={20}
+          y={50}
+          width={100}
+          height={100}
+          fill="red"
+          shadowBlur={10}
+        />
+        <Circle x={200} y={100} radius={50} fill="green" />
+        <Line
+          x={20}
+          y={200}
+          points={[0, 0, 100, 0, 100, 100]}
+          tension={0.5}
+          closed
+          stroke="black"
+          fillLinearGradientStartPoint={{ x: -50, y: -50 }}
+          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+          fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
+        />
+      </Canvas>
     </div>
   );
 }
---
+++ src/components/ServicePane/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css';
+
+const ServicePane = () => {
+  return (
+    <div className="container">
+      <h1>Services</h1>
+
+    </div>
+  )
+}
+
+export default ServicePane;
